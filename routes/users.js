30b6431f296073e1b55d1d7019218fe3349362a0
Michael John Rieser <michi.rieser@gmail.com>
2020-04-12 11:27:37 +0200

    Complete registration

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -36,7 +38,7 @@ router.post(
       user = new User({
         name,
         email,
-        password
+        password,
       });
 
       const salt = await bcrypt.genSalt(10);
@@ -45,8 +47,18 @@ router.post(
 
       await user.save();
 
-      res.send('User saved');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
+      jwt.sign(payload, config.get('jwtSecret'), {
+        expiresIn: 360000
+      }, (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
     } catch (err) {
       console.log(err.message);
       res.status(500).send('Server Error');
