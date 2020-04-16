c3ab33f7fe9d0deb55632f8f6edd23afdec8029f
Michael John Rieser <michi.rieser@gmail.com>
2020-04-16 08:41:34 +0200

    Add login and register component. Add alerts component.

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,4 +1,4 @@
-import React, { useReducuer, useReducer } from 'react';
+import React, { useReducer } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
